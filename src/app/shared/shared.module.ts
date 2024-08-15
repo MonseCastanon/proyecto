fe6223987fbbc53428b86cf9789e1e0bdb5e15f5
UrielYanez <aguayomar77@gmail.com>
2024-08-15 11:30:15 -0600

    Proyecto Funcional

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { Error404PageComponent } from './pages/error404-page/error404-page.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
-=======
-import { FormsModule } from '@angular/forms';
->>>>>>> 8e900c8751e8dde744623760f06303723251aeca
 
 
 
@@ -19,11 +15,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     RouterModule, // Añade RouterModule aquí
-<<<<<<< HEAD
-=======
-    Error404PageComponent,
-    FormsModule
->>>>>>> 8e900c8751e8dde744623760f06303723251aeca
   ]
 })
 export class SharedModule { }
